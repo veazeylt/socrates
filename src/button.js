20c8ed3dcba340bae1a8b6aa89a246eb11d9e863
bishchand <32349708+bishchand@users.noreply.github.com>
2018-11-05 10:45:54 +0800

    Create button.js

---
+++ src/button.js
@@ -0,0 +1,14 @@
+//button to press and generate a name
+
+import React, { Component } from 'react';
+
+class Button extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>Button</div>
+        );
+    }
+}
+ 
+export default Button ;
